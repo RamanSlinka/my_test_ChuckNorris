7c9979f902c69c8e0a9b63cdf0209a23bd52875c
RamanSlinka <romuch1986@gmail.com>
2021-08-15 22:55:36 +0300

    add and refactor styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
 import './App.css';
-import { Pack} from "./t1-ui/ChuckNorrisPage";
+import {ChuckNorrisPage} from "./t1-ui/ChuckNorrisPage";
+import chuckLogo from "../src/common/chucknorris.png"
 
 
 function App() {
-  return (
-    <div className="App">
-      <Pack/>
-    </div>
-  );
+    const chuckLogo1 = chuckLogo;
+    return (
+        <div className="App">
+            <div>
+                < img src={chuckLogo1} alt="ChuckLogo" className="ChuckLogo"/>
+            </div>
+            <ChuckNorrisPage/>
+        </div>
+    );
 }
 
 export default App;
