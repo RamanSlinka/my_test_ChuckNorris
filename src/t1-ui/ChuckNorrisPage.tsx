7682054bb71a482ebf49cf363078a7807810acb1
RamanSlinka <romuch1986@gmail.com>
2021-08-15 22:52:23 +0300

    add InfiniteScroll (+logic)

--- src/t1-ui/ChuckNorrisPage.tsx
+++ src/t1-ui/ChuckNorrisPage.tsx
@@ -1,66 +1,52 @@
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {Content, Loading} from "../App.styles";
 import {fetchCardsTC} from "../t2-bll/reducer";
 import {AppRootStateType} from "../t2-bll/store";
-import User from "./User";
+import Joke from "./Joke";
+import "../App.css"
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
-export const Pack = () => {
+export const ChuckNorrisPage = () => {
 
-    const [page, setPage] = useState(1);
-    const [users, setUsers] = useState<Array<any>>([]);
-    const [loading, setLoading] = useState(true);
-
-
+    const [page, setPage] = useState(0);
+    const [card, setCard] = useState<string[]>([]);
     const dispatch = useDispatch()
 
-     const cardChuck: any = useSelector<AppRootStateType>(state => state.app.value)
-     const imgChuck: any = useSelector<AppRootStateType>(state => state.app.icon_url)
-
-    useEffect(()  => {
-        dispatch(fetchCardsTC())
-    },[page, setPage])
+    const cardChuck = useSelector<AppRootStateType, string>(state => state.app.value)
+    const imgChuck = useSelector<AppRootStateType, string>(state => state.app.icon_url)
 
     useEffect(() => {
+        fetchMoreData()
+    }, [page, setPage])
 
+    useEffect(() => {
         const loadMore = () => {
-            setLoading(true);
-            const newUsers = cardChuck;
-            setUsers((prev: any) => [...prev, newUsers]);
-            setLoading(false);
+            setCard((prev: string[]) => [...prev, cardChuck]);
         }
-        loadMore()
+        cardChuck && loadMore()
     }, [cardChuck, page])
 
 
-
-    const handleScroll = (event: any) => {
-       // console.log('check')
-        dispatch(fetchCardsTC())
-        const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
-
-        if (scrollHeight - scrollTop === clientHeight) {
-            setPage(prev => prev + 1);
+    const fetchMoreData = () => {
+        for (let i = 0; i < 10; i++) {
+            dispatch(fetchCardsTC())
         }
     };
 
-
     return (
+        <div className="wrapperContent">
+            <InfiniteScroll
+                next={fetchMoreData}
+                dataLength={card.length}
+                hasMore={true}
 
+                scrollThreshold={0.95}
+                loader={<h2>Loading...</h2>}
+            >
+                {card.map((el): any => <Joke jokeCard={el} img={imgChuck}/>)}
+            </InfiniteScroll>
 
-        <div className='App'>
-            <button onClick={() => setPage(page + 1)}>Click</button>
-            <Content>
-                <Content onScroll={handleScroll}>
-                    {/*{users && users.map((user: any) => <User key={user.cell} user={user} />)}*/}
-                    {
-                        users.map((el):any =>  <User nameCard={el} img={imgChuck}/>)
-                    }
-
-                </Content>
-                {loading && <Loading>Loading ...</Loading>}
-            </Content>
         </div>
     )
 }
